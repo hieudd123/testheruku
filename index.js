a88b3d523a780bd7e57d2630c8fd6118c07192d7
hieudd123 <nguyen0trung0hieu@gmail.com>
2024-02-26 21:40:26 +0700

    hieu add html

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("tesst heruku")
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+    res.render('index', {
+      domain: domain
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
