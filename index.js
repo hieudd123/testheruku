3aefd51171976b041ba1052d951d9a6384b8448a
hieudd123 <nguyen0trung0hieu@gmail.com>
2024-02-27 13:52:55 +0700

    Update index.js
    add dissconect

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ io.on('connection', (socket) => {
         console.log(`Clent gui`,data);
        //io.emit('data',data) 
     });
+     socket.on('disconnect', () => console.log('Client disconnected'));
 })
 
 //---------------------
