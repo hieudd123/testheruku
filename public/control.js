1cbb54f9800a9004985c9fa96f515ef4d1babe90
hieudd123 <nguyen0trung0hieu@gmail.com>
2024-02-27 11:27:42 +0700

    Update control.js
    add ready

--- public/control.js
+++ public/control.js
@@ -1,10 +1,18 @@
-var socket = io();
+var socket ;
+$( document ).ready(function() {
+    socket = io();
 		socket.on('connect', function(){
 			console.log('Client has connected to the server!');
-		});	  
+		});	
+	
 		socket.on('disconnect', function(){
 			console.log('The client has disconnected!');
 		});
-socket.on('serverdata',(data)=>{
-	console.log('serverdata',data);
-})
+	
+		socket.on('serverdata',(data)=>{
+			console.log('serverdata',data);
+		});
+		socket.on('data',(data)=>{
+			console.log('data',data);
+		})
+});
