0a30e14dfe4e165ff7c8315793fd7d652018ef63
hieudd123 <nguyen0trung0hieu@gmail.com>
2024-02-27 14:19:21 +0700

    Update control.js
    show data

--- public/control.js
+++ public/control.js
@@ -2,18 +2,18 @@ var socket ;
 $( document ).ready(function() {
      socket = io();
 	console.log("ready");
-		//socket.on('connect', function(){
-		//	console.log('Client has connected to the server!');
-		//});	
+		socket.on('connect', function(){
+			console.log('Client has connected to the server!');
+		});	
 	
-		//socket.on('disconnect', function(){
-		//	console.log('The client has disconnected!');
-		//});
+		socket.on('disconnect', function(){
+			console.log('The client has disconnected!');
+		});
 	
-		//socket.on('serverdata',(data)=>{
-		//	console.log('serverdata',data);
-		//});
-		//socket.on('data',(data)=>{
-		//	console.log('data',data);
-		//})
+		socket.on('serverdata',(data)=>{
+			console.log('serverdata',data);
+		});
+		socket.on('data',(data)=>{
+			console.log('data',data);
+		})
 });
